0c714611d3a62fe6b4dcd7e27632f7fa4664e747
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-09-11 15:28:07 +0200

    wip for minification and dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
 	'use strict';
+	var scriptExtractor = require('script-extractor');
+
 
 	// Project configuration.
 	grunt.initConfig({
@@ -11,15 +13,7 @@ module.exports = function (grunt) {
 			},
 			build: {
 				files: {
-					'dist/saillogs.min.js': [
-						'js/lib/Leaflet/leaflet.js',
-						'js/lib/Leaflet.draw/leaflet-draw.js',
-						'js/lib/jquery-*.min.js',
-						'js/lib/jquery.scrollTo.js',
-						'js/modal.js',
-						'js/CalendarControl.js',
-						'js/*.js'
-					]
+					'dist/saillogs.min.js': scriptExtractor('index.html')
 				}
 			}
 		},
@@ -38,6 +32,7 @@ module.exports = function (grunt) {
 		'saillog-thumbs': {
 			files: {
 				src: ['data/*']
+
 			}
 		},
 		connect: {
@@ -54,9 +49,20 @@ module.exports = function (grunt) {
 				files: {
 					"style/saillog.css": "style/saillog.less"
 				}
+			},
+			dist: {
+				files: {
+					"dist/style/saillog.css": "style/saillog.less"
+				}
 			}
 		},
 		watch: {
+			html: {
+				files: ['*.html'],
+				options: {
+					livereload: true
+				}
+			},
 			js: {
 				files: ['js/*.js'],
 				tasks: ['jshint'],
@@ -66,11 +72,36 @@ module.exports = function (grunt) {
 			},
 			less: {
 				files: ['style/*.less'],
-				tasks: ['less'],
+				tasks: ['less:development'],
 				options: {
 					spawn: false,
 					livereload: true
 				}
+			}
+		},
+		copy: {
+			dist: {
+				options: {
+					processContent: function (content, srcpath) {
+						if (srcpath !== 'index.html') {
+							return content;
+						}
+						return content
+							.replace(/(<script(.*)? src="(.*)"><\/script>(\n)?)/g, '')
+							.replace('</body>', '\t<script src="' +'dist/saillogs.min.js' + '"></script>\n</body>');
+					}
+				},
+				files: [
+					{
+						src: [
+							'index.html',
+							'data/*.geojson',
+							'data/index.json',
+							'data/*/*.*'
+						],
+						dest: 'dist/'
+					}
+				]
 			}
 		}
 	});
@@ -94,7 +125,14 @@ module.exports = function (grunt) {
 	});
 	grunt.registerTask('all-ships', ships);
 
-	grunt.registerTask('default', ['less', 'jshint']);
+	grunt.registerTask('default', ['less:development', 'jshint']);
+
+	grunt.registerTask('dist', [
+		'uglify',
+		'less:dist',
+		'copy'
+	]);
+
 	grunt.registerTask('dev', [
 		'default',
 		'connect:server',
